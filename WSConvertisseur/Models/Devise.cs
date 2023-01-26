227599e8c0cf24902bb69d8b6cf41f0327932465
Jimmy LEICHTMANN <jimmy.leichtmann@etu.univ-smb.fr>
2023-01-26 08:31:15 +0100

    Ajout du modèle Devise

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,26 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string nomDevise;
+        private double taux;
+
+        public int Id { get => id; set => id = value; }
+        public string NomDevise { get => nomDevise; set => nomDevise = value; }
+        public double Taux { get => taux; set => taux = value; }
+
+
+        public Devise()
+        {
+                
+        }
+
+        public Devise(int id, string nomDevise, double taux)
+        {
+            Id = id;
+            NomDevise = nomDevise;
+            Taux = taux;
+        }
+    }
+}
